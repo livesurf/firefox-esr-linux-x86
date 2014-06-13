73cc084a7944d688e582a2ba142215bfc39e5c35
pro <pro@ubuntu>
2014-06-13 23:18:45 +0800

    修改：不用lzma打deb包，否则打包太慢 修正：Conflicts Breaks Replaces 加上 firefox-locale-zh-hans，否则导致运行时解析XML错误 修正：去掉 postinst 和 preinst 脚本中的 ${MOZ_LIBDIR}/distribution 部分，否则导致安装时错误

--- mkdeb.sh
+++ mkdeb.sh
@@ -4,10 +4,11 @@ cd ./root
 SIZE=`du -s etc usr | awk '{ sum+=$1} END {print sum}'`
 sed -i "s/^Installed-Size.*/Installed-Size: $SIZE/g" DEBIAN/control
 md5sum `find etc usr -type f | sort` > DEBIAN/md5sums
+NAME=`grep '^Package:' DEBIAN/control | cut -d ' ' -f 2`
 VER=`grep '^Version:' DEBIAN/control | cut -d ' ' -f 2`
 ARCH=`grep '^Architecture:' DEBIAN/control| cut -d ' ' -f 2`
 
 cd -
 
-fakeroot dpkg-deb -Z lzma -b ./root ../firefox-esr_${VER}_${ARCH}.deb 
+fakeroot dpkg-deb -b ./root ../${NAME}_${VER}_${ARCH}.deb
 
